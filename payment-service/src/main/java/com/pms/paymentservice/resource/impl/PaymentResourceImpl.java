2c046f72fcc2651edd83f4faf56010f18aa0399d
Phillip Marques <phillipmarq@hotmail.com>
2023-07-09 18:23:56 -0300

    create payment topic

--- payment-service/src/main/java/com/pms/paymentservice/resource/impl/PaymentResourceImpl.java
+++ payment-service/src/main/java/com/pms/paymentservice/resource/impl/PaymentResourceImpl.java
@@ -1,20 +1,26 @@
 package com.pms.paymentservice.resource.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pms.paymentservice.model.Payment;
 import com.pms.paymentservice.resource.PaymentResource;
+import com.pms.paymentservice.services.PaymentService;
 
 @RestController
 @RequestMapping(value = "/payment")
 public class PaymentResourceImpl implements PaymentResource{
 
+	@Autowired
+	private PaymentService paymentService;
+	
 	@Override
 	public ResponseEntity<Payment> payment(Payment payment) {
-		// TODO Auto-generated method stub
-		return null;
+		paymentService.sendPayment(payment);
+		return ResponseEntity.status(HttpStatus.CREATED).build();
 	}
 
 }
